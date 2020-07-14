aeaca7895a9db19de73f86bae7eba30db43247be
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-14 04:09:31 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,9 @@
-def square_array(array)	
- new_numbers = [1,2,3]
- numbers.each do |num|
- new_numbers << (num ** 2)
-  end
-  return new_numbers
+def square_array(array)
+
+ new_array = []
+
+ array.each do |integer|
+  new_array << integer ** 2
+ end
+ return new_array
 end
